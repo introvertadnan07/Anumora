a9641f7ac1c1875bdca87193694accc75e4ff4af
Md Adnan qaisar <staryounger012@gmail.com>
2026-03-10 00:23:30 +0530

    fix

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -1,11 +1,11 @@
-aconst nodemailer = require("nodemailer");
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS, // Gmail App Password
-  },
+    pass: process.env.EMAIL_PASS
+  }
 });
 
 module.exports = transporter;

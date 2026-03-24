8fe37274fb2c1526726ff2211ee9f3b52ebfb630
introvertadnan07 <staryounger012@gmail.com>
2026-03-24 21:53:10 +0530

    fix bugs

--- cloudConfig.js
+++ cloudConfig.js
@@ -5,19 +5,17 @@ cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
-
 });
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-      folder: 'wanderlust_DEV',
-      allowerdFormats: ["png", "jpg", "jpeg"],
-
+        folder: 'wanderlust_DEV',
+        allowedFormats: ["png", "jpg", "jpeg"],
     },
-  });
+});
 
-  module.exports ={
+module.exports = {
     cloudinary,
     storage,
-  };
+};
